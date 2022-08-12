6bc87e909e40e0954e18dbbaf30d40d751d95248
Robbie Prokop <robbieprokop@gmail.com>
2022-08-12 14:37:07 -0500

    uniItem redirect

--- frontend/src/components/UniItem.jsx
+++ frontend/src/components/UniItem.jsx
@@ -1,7 +1,18 @@
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
-const UniItem = () => {
-  return <div>UniItem</div>;
+const UniItem = ({ uni }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <a href={`${uni.web_pages[0]}`} target="_blank">
+        <div className="bg1">
+          <h2>Battery</h2>
+          <p>Respiration</p>
+        </div>
+      </a>
+    </>
+  );
 };
 export default UniItem;
