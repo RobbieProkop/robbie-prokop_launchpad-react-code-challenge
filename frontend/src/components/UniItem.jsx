4065524310891ffe647ed474ec997bec308217ea
Robbie Prokop <robbieprokop@gmail.com>
2022-08-12 17:58:45 -0500

    styling the grid

--- frontend/src/components/UniItem.jsx
+++ frontend/src/components/UniItem.jsx
@@ -2,21 +2,15 @@ import { useSelector } from "react-redux";
 
 const UniItem = ({ uni, id }) => {
   return (
-    <>
-      <a
-        href={`${uni.web_pages[0]}`}
-        target="_blank"
-        className={id % 2 ? "bg1" : "bg2"}
-      >
-        <div>
-          <h2>{uni.name}</h2>
-          <a href={`${uni.web_pages[0]}`} target="_blank">
-            {uni.web_pages[0]}
-          </a>
-          <p>{uni.country}</p>
-        </div>
-      </a>
-    </>
+    <a
+      href={`${uni.web_pages[0]}`}
+      target="_blank"
+      className={id % 2 ? "bg1 uni" : "bg2 uni"}
+    >
+      <h3>{uni.name}</h3>
+      <p>{uni.web_pages[0]}</p>
+      <p>{uni.country}</p>
+    </a>
   );
 };
 export default UniItem;
