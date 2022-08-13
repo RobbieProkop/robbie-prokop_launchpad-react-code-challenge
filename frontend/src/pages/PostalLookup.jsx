53a211dac417f41346afcbc66d53787d845b7782
Robbie Prokop <robbieprokop@gmail.com>
2022-08-13 11:17:53 -0500

    start creating PostalLookup page

--- frontend/src/pages/PostalLookup.jsx
+++ frontend/src/pages/PostalLookup.jsx
@@ -1,4 +1,43 @@
+import { toast, ToastContainer } from "react-toastify";
+import Spinner from "../components/Spinner";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 const PostalLookup = () => {
-  return <div>Postal Lookup</div>;
+  const dispatch = useDispatch();
+
+  const { isLoading, isError, message } = useSelector(
+    (state) => state.universities
+  );
+  const [postal, setPostal] = useState("");
+
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="container">
+          <section className="heading">
+            <h1>Local information based on Zip Code</h1>
+            {/* search for Postal/Zip code
+            currently limited to the USA  */}
+            <div className="search postal">
+              <div className="column">
+                <label htmlFor="postal-search">Enter Your Zip Code</label>
+                <input
+                  type="text"
+                  name="uni"
+                  id="uniName"
+                  value={postal}
+                  placeholder="12345"
+                  onChange={(e) => setPostal(e.target.value)}
+                />
+              </div>
+            </div>
+          </section>
+        </div>
+      )}
+    </>
+  );
 };
 export default PostalLookup;
