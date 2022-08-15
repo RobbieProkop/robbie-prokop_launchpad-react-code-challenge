66de035458d7ca8c1c76dcf47c0f1d2bd9a77b78
Robbie Prokop <robbieprokop@gmail.com>
2022-08-15 13:04:42 -0500

    returned to prev search without dropdown.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,24 +20,14 @@ const Home = () => {
   //useStates
   const [searchId, setSearchId] = useState("");
   const [userId, setUserId] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
 
-  //events
+  //Events
+  //submit post
   const addPostHandler = (e) => {
     e.preventDefault();
     {
       !togglePostForm && dispatch(setToggleForm());
     }
-  };
-  const filterHandler = (e, data) => {
-    const searchWord = e.target.value;
-    (data = "userId") ? setUserId(searchWord) : setSearchId(searchWord);
-    const newFilter = posts.filter((post) => {
-      data = "userId"
-        ? post.userId === Number(searchWord)
-        : post.id === Number(searchWord);
-    });
-    setFilteredData(newFilter);
   };
 
   useEffect(() => {
@@ -51,25 +41,6 @@ const Home = () => {
     //to be used if i want to reset state after moving to another page
     // return () => dispatch(reset());
   }, [dispatch, isError, message]);
-
-  // for user search results
-  let users = [];
-  posts.map((post) => {
-    if (!users.includes(Number(post.userId))) {
-      users.push(post.userId);
-    } else {
-      return users;
-    }
-  });
-  console.log(users);
-
-  // for id search results
-  let ids = [];
-  posts.map((post) => {
-    if (!ids.includes(Number(post.id))) {
-      ids.push(post.id);
-    }
-  });
 
   return (
     <>
@@ -93,47 +64,22 @@ const Home = () => {
                   id="text"
                   value={userId}
                   placeholder="User:"
-                  onChange={(e) => filterHandler(e, "userId")}
+                  onChange={(e) => setUserId(e.target.value)}
                   disabled={searchId}
                 />
-                {filteredData.length != 0 && (
-                  // { userId.length != 0 && (
-                  <div className="data-result">
-                    {filteredData.map((user) => (
-                      <a key={user} className="data-item">
-                        <p>{user}</p>
-                      </a>
-                    ))}
-                  </div>
-                )}
               </div>
 
               <div className="column">
                 <label htmlFor="postSearch">Search by ID</label>
                 <input
-                  type="number"
+                  type="text"
                   name="searchId"
                   id="text"
                   value={searchId}
                   placeholder="ID:"
-                  onChange={(e) => filterHandler(e, "searchId")}
+                  onChange={(e) => setSearchId(e.target.value)}
                   disabled={userId}
                 />
-                {filteredData.length != 0 && (
-                  <div className="data-result">
-                    {filteredData
-                      // .filter((post) => {
-                      //   if (post.userId === Number(userId)) {
-                      //     return post;
-                      //   }
-                      // })
-                      .map((id) => (
-                        <a key={id} className="data-item">
-                          <p>{id}</p>
-                        </a>
-                      ))}
-                  </div>
-                )}
               </div>
             </div>
           </section>
