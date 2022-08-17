d1643c8cf686f48755e1878882b45a64ddd7ea9b
Robbie Prokop <robbieprokop@gmail.com>
2022-08-17 17:13:52 -0500

    complete home page tests

--- frontend/cypress/e2e/home.spec.cy.js
+++ frontend/cypress/e2e/home.spec.cy.js
@@ -4,77 +4,77 @@ describe("Home Page", () => {
     cy.visit("/");
   });
 
-  //add post btn
-  // context("form", () => {
-  //   beforeEach(() => {
-  //     cy.contains("Add a post!").click();
-  //   });
-  //   describe("filling in the form", () => {
-  //     it("should add text to userID input", () => {
-  //       cy.get("#userId").click().type("1");
-  //     });
-  //     it("should add text to Title input", () => {
-  //       cy.get("#title").click().type("1");
-  //     });
-  //     it("should add text to Post input", () => {
-  //       cy.get("#post-form").click().type("1");
-  //     });
-  //   });
-  //   describe("show error for incomplete fields", () => {
-  //     it("should try to submit incomplete form", () => {
-  //       cy.get("#form-submit").click();
-  //     });
-  //     it("should find the toast error - fill in form", () => {
-  //       cy.get("#form-submit")
-  //         .click()
-  //         .get(".Toastify__toast-body > :nth-child(2)");
-  //     });
-  //   });
-  //   it("should find and close the close button", () => {
-  //     cy.get(".form-group span", { timeout: 10000 }).click();
-  //   });
-  // });
+  // add post btn
+  context("form", () => {
+    beforeEach(() => {
+      cy.contains("Add a post!").click();
+    });
+    describe("filling in the form", () => {
+      it("should add text to userID input", () => {
+        cy.get("#userId").click().type("1");
+      });
+      it("should add text to Title input", () => {
+        cy.get("#title").click().type("1");
+      });
+      it("should add text to Post input", () => {
+        cy.get("#post-form").click().type("1");
+      });
+    });
+    describe("show error for incomplete fields", () => {
+      it("should try to submit incomplete form", () => {
+        cy.get("#form-submit").click();
+      });
+      it("should find the toast error - fill in form", () => {
+        cy.get("#form-submit")
+          .click()
+          .get(".Toastify__toast-body > :nth-child(2)");
+      });
+    });
+    it("should find and close the close button", () => {
+      cy.get(".form-group span", { timeout: 10000 }).click();
+    });
+  });
   context("search", () => {
-    // describe("search UserId", () => {
-    //   beforeEach(() => {
-    //     cy.get("#search-user").click().type("2");
-    //   });
-    //   it("should find search user input", () => {
-    //     cy.get("#search-user").click().type("2");
-    //   });
-    //   it("should find posts by user: 2", () => {
-    //     cy.contains("User: 2");
-    //     expect(".posts").to.not.contains("User: 1");
-    //   });
-    //   it("should backspace input", () => {
-    //     cy.get("#search-user").type("{backspace}");
-    //   });
-    // });
-    // describe("search postId", () => {
-    //   beforeEach(() => {
-    //     cy.get("#search-postId").click().type("2");
-    //   });
-    //   it("should find search user input", () => {
-    //     cy.get("#search-postId").click().type("2");
-    //   });
-    //   it("should find posts by user: 2", () => {
-    //     cy.contains("User: 1");
-    //   });
-    //   it("should have only 1 post match post id", () => {
-    //     cy.get(".posts").children().should("have.length", 1);
-    //   });
-    // });
-    // describe("return to top btn", () => {
-    //   beforeEach(() => {
-    //     cy.scrollTo(0, 100000);
-    //   });
-    //   it("should click return to top btn", () => {
-    //     cy.get(".return-to-top").click();
-    //   });
-    //   it("should see search bars", () => {
-    //     cy.get("#search-user").should("be.visible");
-    //   });
-    // });
+    describe("search UserId", () => {
+      beforeEach(() => {
+        cy.get("#search-user").click().type("2");
+      });
+      it("should find search user input", () => {
+        cy.get("#search-user").click().type("2");
+      });
+      it("should find posts by user: 2", () => {
+        cy.contains("User: 2");
+        expect(".posts").to.not.contains("User: 1");
+      });
+      it("should backspace input", () => {
+        cy.get("#search-user").type("{backspace}");
+      });
+    });
+    describe("search postId", () => {
+      beforeEach(() => {
+        cy.get("#search-postId").click().type("2");
+      });
+      it("should find search user input", () => {
+        cy.get("#search-postId").click().type("2");
+      });
+      it("should find posts by user: 2", () => {
+        cy.contains("User: 1");
+      });
+      it("should have only 1 post match post id", () => {
+        cy.get(".posts").children().should("have.length", 1);
+      });
+    });
+    describe("return to top btn", () => {
+      beforeEach(() => {
+        cy.scrollTo(0, 100000);
+      });
+      it("should click return to top btn", () => {
+        cy.get(".return-to-top").click();
+      });
+      it("should see search bars", () => {
+        cy.get("#search-user").should("be.visible");
+      });
+    });
     describe("edit and delete", () => {
       describe("edit btn", () => {
         beforeEach(() => {
